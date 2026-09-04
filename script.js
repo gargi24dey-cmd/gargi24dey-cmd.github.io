67fc8ba8d1c0fb88cf6855bf35b9441349263f33
gargi24dey-cmd <gargi24.dey@gmail.com>
2026-09-04 16:56:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,22 @@ fetch(`https://api.github.com/users/${username}/repos?sort=updated`)
     container.innerHTML = "<p>Couldn't load projects right now.</p>";
     console.error(error);
   });
+const toggleBtn = document.getElementById("theme-toggle");
+
+// Apply saved theme on page load
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggleBtn.textContent = "☀️ Light Mode";
+}
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "☀️ Light Mode";
+    localStorage.setItem("theme", "dark");
+  } else {
+    toggleBtn.textContent = "🌙 Dark Mode";
+    localStorage.setItem("theme", "light");
+  }
+});
