d134f4709f2210fdbd7ca5cb193ec442b2574f2b
gargi24dey-cmd <gargi24.dey@gmail.com>
2026-09-25 12:06:50 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -2,16 +2,16 @@ const toggleBtn = document.getElementById("theme-toggle");
 
 if (localStorage.getItem("theme") === "dark") {
   document.body.classList.add("dark-mode");
-  toggleBtn.textContent = "☀️ Light Mode";
+  toggleBtn.textContent = "☀️";
 }
 
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   if (document.body.classList.contains("dark-mode")) {
-    toggleBtn.textContent = "☀️ Light Mode";
+    toggleBtn.textContent = "☀️";
     localStorage.setItem("theme", "dark");
   } else {
-    toggleBtn.textContent = "🌙 Dark Mode";
+    toggleBtn.textContent = "🌙";
     localStorage.setItem("theme", "light");
   }
 });
